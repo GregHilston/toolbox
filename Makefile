9616b80d811fdece1a375dfaa957940b82e88607
David Harrigan <dharrigan@dyn.com>
2015-09-28 23:48:14 -0400

    big update

--- Makefile
+++ Makefile
@@ -1,3 +1,10 @@
+install: install_dots install_bin
+
+install_dots: backup
+	./install/install_dots.sh
+
+install_bin:
+	./install/install_bin.sh
 
 backup:
 	./install/backup.sh
--- install/backup.sh
+++ install/backup.sh
@@ -1,16 +1,27 @@
 #!/usr/bin/env bash
+set -e
+
 SCRIPT_HOME=$(cd "$(dirname $0)" && pwd)
 TOOLBOX_HOME=$(echo $SCRIPT_HOME | rev | cut -d/ -f2- | rev)
 
 BACKUP_DIR=$TOOLBOX_HOME/backup
-THIS_BACK_UP=$BACKUP_DIR/backup-$(date +%F-%T)
-FILES_TO_BACKUP=(bashrc gitignore tmux.conf vimrc zshrc)
-DIRS_TO_BACKUP=(env oh-my-zsh vim zsh)
+THIS_BACKUP=$BACKUP_DIR/backup-$(date +%F-%T)
+BACKUPS=(bashrc gitignore tmux.conf vimrc zshrc env vim)
 
 if [ ! -d $BACKUP_DIR ]; then
   echo Create $BACKUP_DIR
-  mkdir $BACKUP_DIR
+  mkdir "$BACKUP_DIR"
 fi
 
-echo Backing up current configuration into [$THIS_BACK_UP]
-mkdir $THIS_BACKUP
+echo Backing up current configuration into [$THIS_BACKUP]
+mkdir "$THIS_BACKUP"
+
+result="Backed up: "
+for backup_file in "${BACKUPS[@]}"; do
+  if [ -f "~/.$backup_file" ]; then
+    cp "~/.$backup_file" "$THIS_BACKUP/$backup_file"
+    $result="$result $backup_file"
+  fi
+done
+
+echo $result
