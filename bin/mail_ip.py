9812f1ed784f20aed8c924772d0df574db78a507
GregHilston <Gregory.Hilston@gmail.com>
2019-05-06 13:31:58 -0600

    Fixed mail ip, need to be able to set it up to run on boot

--- bin/mail_ip.py
+++ bin/mail_ip.py
@@ -1,15 +1,31 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3.7
 
 import subprocess
 import smtplib
 from email.mime.text import MIMEText
 import datetime
+import sys, os
+import socket
 
 to = 'Grehgh@gmail.com'
 gmail_user = 'grehgpi@gmail.com'
 gmail_password = ''
 
-with open ("../secrets/gmail_password.conf.personal", "r") as myfile:
+def get_ip_address():
+    sh_command = "ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'"
+
+    ps = subprocess.Popen(sh_command,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+    byte_output = ps.communicate()[0]
+    string_output = byte_output.decode("utf-8")
+
+    return string_output
+
+def get_host_name():
+    return socket.gethostname()
+
+# so we can get path based on where this script is located
+# and not from where we execute it from
+with open (f"{os.path.dirname(sys.argv[0])}/../secrets/gmail_password.conf.personal", "r") as myfile:
     gmail_password=myfile.read().replace('\n', '')
 
 smtpserver = smtplib.SMTP('smtp.gmail.com',587)
@@ -17,15 +33,12 @@ smtpserver.ehlo()
 smtpserver.starttls()
 smtpserver.ehlo
 smtpserver.login(gmail_user, gmail_password)
-today = datetime.date.today()
-arg='ip route list'
-p=subprocess.Popen(arg,shell=True,stdout=subprocess.PIPE)
-data = p.communicate()
-split_data = data[0].split()
-print(split_data)
-ipaddr = split_data[split_data.index('src')+1]
-my_ip = 'Your ip is %s' % ipaddr
-msg = MIMEText(my_ip)
+
+body = ''
+body += f"IP Address = {get_ip_address()}"
+body +=  f"hostname = {get_host_name()}"
+
+msg = MIMEText(body)
 msg['Subject'] = 'IP For RaspberryPi'
 msg['From'] = gmail_user
 msg['To'] = to
