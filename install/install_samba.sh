3f062d3bb18a5df6587533ebd73e26b4ea5c71c5
GregHilston <Gregory.Hilston@gmail.com>
2019-05-06 13:28:48 +0100

    updated samba script to be real samba mount name

--- install/install_samba.sh
+++ install/install_samba.sh
@@ -3,7 +3,7 @@
 # first make a mount point
 mkdir -p /media/mothership
 
-LINE='//retropie/public /media/mothership cifs guest,uid=1000,iocharset=utf8 0 0'
+LINE='//retropie/mothership /media/mothership cifs guest,uid=1000,iocharset=utf8 0 0'
 FILE='/etc/fstab'
 grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
 
