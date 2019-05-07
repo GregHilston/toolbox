9063a782622c8050c95d06adfe6c4b9873fae0f4
GregHilston <Gregory.Hilston@gmail.com>
2019-05-06 18:02:36 -0600

    Supporting ipv6, if /sbin/ip available, and subject has hostname

--- bin/mail_ip.py
+++ bin/mail_ip.py
@@ -11,8 +11,17 @@ to = 'Grehgh@gmail.com'
 gmail_user = 'grehgpi@gmail.com'
 gmail_password = ''
 
-def get_ip_address():
+def get_ipv4_address():
     sh_command = "ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'"
+
+    ps = subprocess.Popen(sh_command,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+    byte_output = ps.communicate()[0]
+    string_output = byte_output.decode("utf-8")
+
+    return string_output
+
+def get_ipv6_address():
+    sh_command = "/sbin/ip -6 addr | grep inet6 | awk -F '[ \t]+|/' '{print $3}' | grep -v ^::1 | grep -v ^fe80"
 
     ps = subprocess.Popen(sh_command,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
     byte_output = ps.communicate()[0]
@@ -35,11 +44,12 @@ smtpserver.ehlo
 smtpserver.login(gmail_user, gmail_password)
 
 body = ''
-body += f"IP Address = {get_ip_address()}"
+body += f"IPV4 Address = {get_ipv4_address()}"
+body += f"IPV6 Address = {get_ipv6_address()}"
 body +=  f"hostname = {get_host_name()}"
 
 msg = MIMEText(body)
-msg['Subject'] = 'IP For RaspberryPi'
+msg['Subject'] = f"{get_host_name()} powered on with internet"
 msg['From'] = gmail_user
 msg['To'] = to
 smtpserver.sendmail(gmail_user, [to], msg.as_string())
