a7cac5a5abb9eb93456ba68da47cd71e6df746f1
GregHilston <Gregory.Hilston@gmail.com>
2019-05-21 03:38:10 +0100

    Fixed zsh by exporting needed variable and added debug prints to autocomplete, now working

--- install/install_zsh_autocomplete.sh
+++ install/install_zsh_autocomplete.sh
@@ -1,11 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -e
+
 # Need to install the zsh-autosuggestions ourselves, no longer shipped
 # used https://gist.github.com/dogrocker/1efb8fd9427779c827058f873b94df95
 # and http://www.geekmind.net/2011/08/how-to-reload-your-zshrc.html
 # and https://stackoverflow.com/questions/36498981/shell-dont-fail-if-git-clone-if-folder-already-exists
 if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ] ; then
+    # echo "attempting to clone to $ZSH_CUSTOM/plugins/zsh-autosuggestions"
     git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-else
+else 
+    # echo "attempting to update $ZSH_CUSTOM/plugins/zsh-autosuggestions"
     pushd "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
     git pull origin master
 fi
