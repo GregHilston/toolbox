5c7dcfe2b1655d307fb6bc3cc6c34cf3936071f8
GregHilston <Gregory.Hilston@gmail.com>
2019-08-29 15:49:11 -0600

    got DS setup example for four different windows

--- bin/session_ds_corpus.sh
+++ bin/session_ds_corpus.sh
@@ -3,19 +3,25 @@
 SESSION_NAME="ds_corpus"
 CARRAIGE_RETURN="C-m"
 ABSOLUTE_PATHS_TO_OPEN=(
+  "api"
   "~/.captain/headproxy/ds-most-important-phrases-in-corpus-api"
+  "drone"
   "~/.captain/headproxy/ds-most-important-phrases-in-corpus-drone"
+  "failed-to-send-drone"
+  "~/.captain/headproxy/ds-most-important-phrases-in-corpus-failed-to-send-drone"
+  "flask-testbed"
+  "~/Git/ds-flask-testbed"
 )
 tmux has-session -t ${SESSION_NAME}
 
 if [ $? != 0 ];then
   # Create the new session
-  tmux new-session -s ${SESSION_NAME} -n ds_corpus -d
+  tmux new-session -s ${SESSION_NAME} -n "${ABSOLUTE_PATHS_TO_OPEN[0]}" -d
 
   # Loop through all paths to open
-  for path in "${ABSOLUTE_PATHS_TO_OPEN[@]}"; do
-    tmux send-keys -t ${SESSION_NAME} "cd $path" ${CARRAIGE_RETURN}
-    tmux new-window -n bash -t ${SESSION_NAME}
+  for (( i=1; i<${#ABSOLUTE_PATHS_TO_OPEN[@]} ; i+=2 )) ; do
+    tmux send-keys -t ${SESSION_NAME} "cd ${ABSOLUTE_PATHS_TO_OPEN[i]}" ${CARRAIGE_RETURN}
+    tmux new-window -n "${ABSOLUTE_PATHS_TO_OPEN[i+1]}" -t ${SESSION_NAME}
   done
 
   # kills the last window made, that was extra cause of our for loop
