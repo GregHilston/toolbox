250413618a80700b1c8515a53b5e8213c8390181
GregHilston <Gregory.Hilston@gmail.com>
2019-09-20 14:54:31 -0600

    example on GCP and sourcing on zsh

---
+++ bin/gcp/insert_dummy_table_to_table_via_cli.sh
@@ -0,0 +1,1 @@
+gcloud spanner rows insert --instance=ad-infrastructure --database=user_data --table=AccountPos --data=AccountId=MDAwNTI4MjYtMjUzNi00NjUwLTk0OGEtZjVmNzI0MDIzZDNj,PosId=MQ==,PosName="Grehg",PosNumber=1234
