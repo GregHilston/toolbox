4f3917827f5a1b6627566e8089d2a7299432cd4c
GregHilston <Gregory.Hilston@gmail.com>
2019-09-23 11:55:58 -0600

    Made useful script to setup tmux session with windows running commands

--- bin/session_ds_cluster.sh
+++ bin/session_ds_cluster.sh
@@ -1,45 +1,53 @@
 #!/bin/sh
 
-SESSION_NAME="ds_cluster"
-CARRAIGE_RETURN="C-m"
-NAME_OR_COMMANDS_TO_RUN=(
+session_name="ds_cluster"
+carriage_return="C-m"
+name_or_commands_to_run=(
   "winterfell"
   "ssh winterfell"
   "harrenhal"
   "ssh harrenhal"
+  "reach"
+  "ssh reach"
 )
-DEFAULT_WINDOW_NAME=0
+default_window_name=0
 
-echo "checking if $SESSION_NAME already exists"
-tmux has-session -t $SESSION_NAME
+# check if session already exists
+tmux has-session -t $session_name
 
 if [ $? != 0 ];then
-  # Create the new session
-  echo "creating session named $SESSION_NAME, as it doesn't exist"
-  tmux new-session -s $SESSION_NAME
+  # Create the new session if it does not exist
+  tmux new-session -d -A -s $session_name
+  # Loop through name and commands to run
+  for (( i=0; i<${#name_or_commands_to_run[@]} ; i+=2 )) ; do
+    window_name=${name_or_commands_to_run[i]}
+    command=${name_or_commands_to_run[i+1]}
 
-  # Loop through all paths to open
-  for (( i=0; i<${#NAME_OR_COMMANDS_TO_RUN[@]} ; i+=2 )) ; do
-    window_name=${NAME_OR_COMMANDS_TO_RUN[i]}
-    command=${NAME_OR_COMMANDS_TO_RUN[i+1]}
-
+    if (($i == 0));then
+      first_window_name=$window_name
+    fi
     # create window
-    echo "creating window named $window_name on session $SESSION_NAME"
-    tmux new-window -n $window_name -t $SESSION_NAME
+    echo "creating window named $window_name on session $session_name"
+    tmux new-window -n $window_name -t $session_name
 
     # rename window
-    echo "renaming window from $DEFAULT_WINDOW_NAME to $window_name"
-    tmux rename-window -t $DEFAULT_WINDOW_NAME $window_name
+    echo "renaming window to $window_name"
+    tmux rename-window $window_name
 
     # run command
     echo "running command $command"
-    tmux send-keys -t $SESSION_NAME "$command" $CARRAIGE_RETURN
+    tmux send-keys -t $session_name "$command" $carriage_return
   done
 
+  # kill default window
+  echo "killing default window $default_window_name"
+  tmux kill-window -t $default_window_name
+
   # Start out on the first window when we attach
-  # tmux select-window -t ${SESSION_NAME}:0
+  echo "selecting-window first window $session_name:$first_window_name"
+  tmux select-window -t $session_name:$first_window_name
 fi
 
-# Attach to the newly created or previously existed session
-echo "attaching to session named $SESSION_NAME"
-tmux attach -t $SESSION_NAME
+# attach to built up session
+echo "attaching to $session_name"
+tmux attach-session -t $session_name
