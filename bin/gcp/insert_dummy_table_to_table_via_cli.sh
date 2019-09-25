167e2670016f90007f311009ff4bfccd23de0fe1
GregHilston <Gregory.Hilston@gmail.com>
2019-09-25 14:59:47 -0600

    added some fake dummy data

--- bin/gcp/insert_dummy_table_to_table_via_cli.sh
+++
@@ -1,1 +0,0 @@
-gcloud spanner rows insert --instance=ad-infrastructure --database=user_data --table=AccountPos --data=AccountId=MDAwNTI4MjYtMjUzNi00NjUwLTk0OGEtZjVmNzI0MDIzZDNj,PosId=MQ==,PosName="Grehg",PosNumber=1234
