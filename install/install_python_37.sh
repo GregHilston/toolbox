617d0a321c145a7465a943bfccd5173f0f07ded2
Greg.Hilston <Gregory.Hilston@gmail.com>
2020-05-22 10:57:58 -0400

    got python37

--- install/install_python_37.sh
+++ install/install_python_37.sh
@@ -4,15 +4,17 @@ set -e
 # From https://linuxize.com/post/how-to-install-python-3-7-on-ubuntu-18-04/
 
 # Start by updating the packages list and installing the prerequisites:
-apt update
-apt install software-properties-common -y
+sudo apt update
+sudo apt install software-properties-common -y
 
 # Next, add the deadsnakes PPA to your sources list:
-add-apt-repository ppa:deadsnakes/ppa -y
+sudo add-apt-repository ppa:deadsnakes/ppa -y
 
 # Once the repository is enabled, install Python 3.7 and venv:
-apt install python3.7 python3.7-venv -y
+sudo apt install python3.7 python3.7-venv -y
 
 # At this point, Python 3.7 is installed on your Ubuntu system and ready to be used.
 echo "Python3.7 should be installed, use by running '\$ python3.7'"
 python3.7 --version
+
+echo "We do not update the python alias, as this violates PEP 394, we update the python3 alias in ~/.zshrc. THIS MUST BE SOURCED AFTER RUNNING THIS SCRIPT."
