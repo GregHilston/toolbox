22736cd75a587ef1d6d896e3b80b6bb56704efb2
GregHilston <Gregory.Hilston@gmail.com>
2020-08-09 10:00:23 -0400

    Refatored to only install neovim, its plugins, my dot files, ohmyzsh and my theme

---
+++ install/zsh.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+echo "installing zsh..."
+apt-get install zsh -y
+
+echo "setting zsh as the default shell..."
+chsh -s $(which zsh)
+
+echo "zsh-autosuggestions being cloned or updated at path $ZSH/plugins/zsh-autosuggestions"
+if [ ! -d "$ZSH/plugins/zsh-autosuggestions" ] ; then
+    # echo "attempting to clone to $ZSH_CUSTOM/plugins/zsh-autosuggestions"
+    git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH/plugins/zsh-autosuggestions
+else 
+    # echo "attempting to update $ZSH_CUSTOM/plugins/zsh-autosuggestions"
+    pushd "$ZSH/plugins/zsh-autosuggestions"
+    git pull origin master
+fi
+zsh -c "source ~/.zshrc"
