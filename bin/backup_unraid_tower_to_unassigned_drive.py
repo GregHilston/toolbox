497da68e002fc18d8a34da514d19b50d8c60f705
GregHilston <Gregory.Hilston@gmail.com>
2020-11-21 12:44:22 -0500

    Adds a new script to backup our backup produced by the Backup/Restore application to our local unassigned drive and remote FOB drive

---
+++ bin/backup_unraid_tower_to_unassigned_drive.py
@@ -0,0 +1,13 @@
+import subprocess
+
+from send_message_to_grehg_xyz_slack import send_message_to_grehg_xyz_slack
+
+
+send_message_to_grehg_xyz_slack("#backup", "Rsyncing backup to unassinged drive started")
+
+try:
+    subprocess.call(["rsync", "-azh", "/mnt/user/backup", "pi@fob:/mnt/disks/mothership/backup/"])
+except Exception as e:
+    send_message_to_grehg_xyz_slack("#backup", f"An exception occurred when trying to Rsync our backup to unassigned drive. The exception was {str(e)}")
+
+send_message_to_grehg_xyz_slack("#backup", "Rsyncing backup to unassigned drive ended")
