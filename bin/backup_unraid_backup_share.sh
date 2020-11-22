37372f9214b2c19a7ff1f6f4be782d431863011e
GregHilston <Gregory.Hilston@gmail.com>
2020-11-21 23:37:27 -0500

    Renamed secrets file to be generic and also backing up to unmanaged drive correctly

--- bin/backup_unraid_backup_share.sh
+++ bin/backup_unraid_backup_share.sh
@@ -1,2 +1,2 @@
-python3 backup_unraid_tower_to_fob.py
 python3 backup_unraid_tower_to_unassigned_drive.py
+python3 backup_unraid_tower_to_fob.py
--- bin/backup_unraid_tower_to_unassigned_drive.py
+++ bin/backup_unraid_tower_to_unassigned_drive.py
@@ -6,7 +6,7 @@ from send_message_to_grehg_xyz_slack import send_message_to_grehg_xyz_slack
 send_message_to_grehg_xyz_slack("#backup", "Rsyncing backup to unassinged drive started")
 
 try:
-    subprocess.call(["rsync", "-azh", "/mnt/user/backup", "pi@fob:/mnt/disks/mothership/backup/"])
+    subprocess.call(["rsync", "-azh", "/mnt/user/backup", "/mnt/disks/mothership"])
 except Exception as e:
     send_message_to_grehg_xyz_slack("#backup", f"An exception occurred when trying to Rsync our backup to unassigned drive. The exception was {str(e)}")
 
