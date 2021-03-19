b5e1cc1f0840c9191dc8ea2c94aec3ef9e3b26a0
GregHilston <Gregory.Hilston@gmail.com>
2021-03-19 08:23:15 -0400

    Adds standalone install script for nextcloud client to be able to take notes on new PC

---
+++ install/install_nextcloud_client.sh
@@ -0,0 +1,6 @@
+# From https://www.techrepublic.com/article/how-to-install-the-nextcloud-client-on-ubuntu/
+sudo add-apt-repository ppa:nextcloud-devs/client -y
+
+sudo apt update
+
+sudo apt install nextcloud-client -y
