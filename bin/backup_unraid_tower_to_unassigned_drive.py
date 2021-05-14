dd2e8c1da1c47c80567ecf0761c258d3d1758d6f
GregHilston <Gregory.Hilston@gmail.com>
2021-05-13 23:41:07 -0400

    Correctly sends content of directory or directory to FOB or locally

--- bin/backup_unraid_tower_to_unassigned_drive.py
+++ bin/backup_unraid_tower_to_unassigned_drive.py
@@ -2,7 +2,7 @@ from backup_source_directory_to_destination import backup_source_directory_to_de
 
 
 backup_source_directory_to_destination(
-    source_directory_path="/mnt/user/backup",
+    source_directory_path="/mnt/user/backup", # lack of traling slash means the backup directory will be copied to mothership
     source_directory_name="unraid backup share",
     destination_directory_path="/mnt/disks/mothership",
     destination_directory_name="Unraid external hard drive"
