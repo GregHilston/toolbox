bce087733a2ed58d2a540c9b8adc72d8268bc038
GregHilston <Gregory.Hilston@gmail.com>
2021-12-21 23:02:41 -0500

    feat makes foundation i3 script and moves build script

--- dot/i3-gaps/build-i3-config.sh
+++ dot/i3-gaps/build-i3-config.sh
@@ -4,4 +4,7 @@ IFS=$'\n\t'
 
 
 cat $TOOLBOX_HOME/dot/i3-gaps/config-base \
-    $TOOLBOX_HOME/dot/i3-gaps/config-$(hostname) > $HOME/.config/i3/config
+    $TOOLBOX_HOME/dot/i3-gaps/config-$(hostname) > ./config
+
+
+# $HOME/.config/i3/config
