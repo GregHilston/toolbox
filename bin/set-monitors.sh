fdc7c220f289b66e17536c2cd13479378f94eaa5
GregHilston <Gregory.Hilston@gmail.com>
2022-04-30 11:00:00 -0400

    fixing foundation i3 and scripts after setting up sunrun laptop

--- bin/set-monitors.sh
+++ bin/set-monitors.sh
@@ -2,4 +2,8 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-xrandr --output DP-0 --primary --mode 2560x1440 --pos 1080x0 --rotate normal --output DP-1 --off --output HDMI-0 --off --output DP-2 --mode 1920x1080 --pos 3640x0 --rotate right --output DP-3 --off --output DP-4 --mode 1920x1080 --pos 0x0 --rotate left --output DP-5 --off --output USB-C-0 --off
+# xrandr --output DP-0 --primary --mode 2560x1440 --pos 1080x0 --rotate normal --output DP-1 --off --output HDMI-0 --off --output DP-2 --mode 1920x1080 --pos 3640x0 --rotate right --output DP-3 --off --output DP-4 --mode 1920x1080 --pos 0x0 --rotate left --output DP-5 --off --output USB-C-0 --off
+xrandr \
+    --output DP-0 --primary --mode 1920x1080 --pos 0x0 --rotate left \
+    --output DP-2 --primary --mode 2560x1440 --pos 1080x0 --rotate normal \
+    --output DP-4 --primary --mode 1920x1080 --pos 3640x0 --rotate right
