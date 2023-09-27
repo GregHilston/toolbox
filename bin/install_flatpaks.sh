c34f2baf4161e5c6d444326739d3cc6693946630
GregHilston <Gregory.Hilston@gmail.com>
2023-09-26 22:49:36 -0400

    feat: add installation of vscode via flatpak

--- bin/install_flatpaks.sh
+++ bin/install_flatpaks.sh
@@ -8,4 +8,4 @@ flatpak install flathub com.slack.Slack -y
 flatpak install flathub com.spotify.Client -y
 flatpak install flathub com.bitwarden.desktop -y
 flatpak install flathub md.obsidian.Obsidian -y
-
+flatpak install flathub com.visualstudio.code -y
