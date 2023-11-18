6b4ea50d460d997e67ba6713b2b7e71eeaf2a096
GregHilston <Gregory.Hilston@gmail.com>
2023-11-18 18:37:24 -0500

    fix: docker permissions for flatpak testing

--- docker/docker-run.sh
+++ docker/docker-run.sh
@@ -1,4 +1,9 @@
-# this starts and shell execs a throw away container that will be deleted after stopped
-# for clarity, all changes made outside of the volume'd in toolbox will be lost
-# this is useful for testing out our toolbox
-docker run --rm -it --volume $(pwd):/toolbox --workdir /toolbox --entrypoint /bin/bash toolbox
+# This starts and shell execs a throw away container that will be deleted after
+# stopped for clarity, all changes made outside of the volume'd in toolbox will
+# be lost this is useful for testing out our toolbox.
+
+# --privileged is added so that flatpaks can install in user namespaces. Read
+# here for more information:
+# https://discourse.flathub.org/t/error-running-flatpack-in-dockerfile/1636
+
+docker run --privileged --rm -it --volume $(pwd):/toolbox --workdir /toolbox --entrypoint /bin/bash toolbox
