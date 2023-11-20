6702ef1c6d375c130153f8ae6181a08eec1639a4
GregHilston <Gregory.Hilston@gmail.com>
2023-11-19 19:13:06 -0500

    feat: toolbox supports multiple environments now

--- ansible/README.md
+++ ansible/README.md
@@ -1,3 +1,3 @@
 # Ansible
 
-Houses ansible playbooks, which drive our automations that perform environment customizations. Generally, the main entry point of `deploy.yml` is called via the top level `deploy.sh`.
+Houses ansible playbooks, which drive our automations that perform environment customizations. Generally, the main entry point for each operating system is titled `deploy-{operating-system}.yml`, which is called via the top level `deploy.sh`.
--- install.sh
+++ install.sh
@@ -2,4 +2,45 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-ansible-playbook -i ansible/hosts.ini ansible/playbooks/deploy.yml
+# Directory containing variables files
+variables_dir="./ansible/variables"
+
+# Populate supported operating systems based on available files
+supported_operating_systems=()
+
+for file in "$variables_dir"/*.yml; do
+  # Extract the filename without the path and extension
+  os=$(basename "$file" .yml)
+  supported_operating_systems+=("$os")
+done
+
+# Function to display usage text
+display_usage() {
+  echo "Error: $1"
+  echo "Usage: $0 <operating system (${supported_operating_systems[@]})>"
+}
+
+# Check if the required operating system parameter is provided
+if [ "$#" -ne 1 ]; then
+  display_usage "Missing required operating system parameter."
+  exit 1
+fi
+
+# Set the operating system variable
+OS="$1"
+
+# Validate the supported operating systems
+found=false
+for supported_os in "${supported_operating_systems[@]}"; do
+  if [ "$OS" == "$supported_os" ]; then
+    found=true
+    break
+  fi
+done
+
+if [ "$found" == false ]; then
+  display_usage "Unsupported operating system '$OS'."
+  exit 2
+fi
+
+ansible-playbook -i ansible/hosts.ini ansible/playbooks/deploy-$OS.yml
