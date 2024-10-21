df2aadec9672cc1111325411b049609ba93cc833
GregHilston <Gregory.Hilston@gmail.com>
2024-10-21 05:01:36 -0400

    fix: rebase accidentally added old code

--- nixos/hosts/isengard/default.nix
+++ nixos/hosts/isengard/default.nix
@@ -1,14 +1,10 @@
 { inputs, lib, config, pkgs, vars, ... }:
 
-let
-  # Import common configuration
-  common = import ../../common.nix { inherit config pkgs; };
-
-  # Get the user value from common.nix
-  user = common.user;
-in
 {
   imports = [
     ./hardware-configuration.nix
+    ../../common.nix
   ];
+
+  # You can add isengard-specific configurations here
 }
