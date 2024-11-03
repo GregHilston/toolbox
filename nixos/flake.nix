6ed3f409d4679130aa295cd523c72bed84b7816f
GregHilston <Gregory.Hilston@gmail.com>
2024-11-02 21:54:48 -0400

    fix: ability to deploy

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -41,7 +41,7 @@
             home-manager = {
               useGlobalPkgs = true;
               useUserPackages = true;
-	            backupFileExtension = "backup2";
+	            backupFileExtension = "backup";
             };
           }
         ];
