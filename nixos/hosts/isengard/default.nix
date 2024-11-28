d8e1734aedc80b3305a7f697058799f0e4eed241
GregHilston <Gregory.Hilston@gmail.com>
2024-11-28 03:53:16 -0500

    feat: enable bluetooth for isengard

--- nixos/hosts/isengard/default.nix
+++ nixos/hosts/isengard/default.nix
@@ -15,4 +15,7 @@
   };
   
   powerManagement.powertop.enable = true;
+
+  hardware.bluetooth.enable = true; # enables support for Bluetooth
+  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
 }
