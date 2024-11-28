b235d1845ee03257e082438249fba29091e865a5
GregHilston <Gregory.Hilston@gmail.com>
2024-11-28 04:56:29 -0500

    refactor: leverage config variables

--- nixos/hosts/vms/x86_64/default.nix
+++ nixos/hosts/vms/x86_64/default.nix
@@ -16,4 +16,16 @@
 
   services.spice-vdagentd.enable = true;
   services.qemuGuest.enable = true;
+
+  services.openssh = {
+    enable = true;
+    ports = [ 22 ];
+    settings = {
+      PasswordAuthentication = true;
+      AllowUsers = null; # Allows all users by default. Can be [ "user1" "user2" ]
+      UseDns = true;
+      X11Forwarding = false;
+      PermitRootLogin = "prohibit-password"; # "yes", "without-password", "prohibit-password", "forced-commands-only", "no"
+    };
+  };
 }
