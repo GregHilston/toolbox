fb41b31c6c2a36576c5e7111b57ccad11bb9c84d
GregHilston <Gregory.Hilston@gmail.com>
2024-12-15 22:10:42 -0500

    fix: temporary fix to try to get user to be ghilston

--- nixos/hosts/pcs/foundation/default.nix
+++ nixos/hosts/pcs/foundation/default.nix
@@ -6,4 +6,58 @@
   ];
 
   networking.hostName = "foundation";
+
+  # Ensure both users exist during transition
+  users.users = {
+    nixos = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" "networkmanager" ];
+      # Keep nixos user temporarily
+    };
+    ${vars.user.name} = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" "networkmanager" "docker" ];
+      shell = pkgs.${vars.user.packages.shell};
+      initialPassword = "password";
+    };
+  };
+  
+  # Override common settings that don't work well in WSL
+  services = {
+    xserver.enable = lib.mkForce false;
+    displayManager.sddm.enable = lib.mkForce false;
+    desktopManager.plasma6.enable = lib.mkForce false;
+    pipewire.enable = lib.mkForce false;
+  };
+
+  # Disable unnecessary services for WSL
+  security.rtkit.enable = lib.mkOverride 900 false;
+  virtualisation.docker.enable = true;
+
+  # WSL-specific settings
+  wsl = {
+    enable = true;
+    nativeSystemd = true;
+    defaultUser = "nixos"; #vars.user.name;
+    startMenuLaunchers = true;
+    wslConf = {
+      automount.root = "/mnt";
+      network.generateResolvConf = true;
+    };
+  };
+
+  # Disable Stylix desktop-related targets
+  stylix = {
+    targets.console.enable = false;
+  };
+
+  # Configure home-manager for WSL
+  home-manager = {
+    extraSpecialArgs = {
+      inherit inputs outputs vars;
+    };
+    users.${vars.user.name} = { pkgs, ... }: {
+      imports = [ ../../../modules/home ];
+    };
+  };
 }
