039ecd1ee61729b5d94469887e16c5e4e98ffc18
GregHilston <Gregory.Hilston@gmail.com>
2025-02-12 15:08:17 -0500

    fix: fix home manager version to be at 25.05

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -10,7 +10,7 @@
 
     # Home manager
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager/master"; # eventually replace with github:nix-community/home-manager/release-25.05
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
