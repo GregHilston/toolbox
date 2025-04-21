0b5d387f994af0299eac784fa3b89f2fa5d26b11
ghilston <Gregory.Hilston@gmail.com>
2025-04-20 21:24:33 -0400

    feat: add flake entry for macos

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -3,10 +3,13 @@
 
   inputs = {
     # Nixpkgs
+    # TODO consider setting this to "github:nixos/nixpkgs/nixpkgs-unstable"; instead, IE unstable and no version directly mentioned, IE latest
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-24.11";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
     # Home manager
     home-manager = {
@@ -21,7 +24,7 @@
     nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
   };
 
-  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, nixos-wsl, nixos-hardware, home-manager, ... }:
+  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, nixos-wsl, nixos-hardware, home-manager, nix-darwin, ... }:
     let
       vars = import ./config/vars.nix { inherit (nixpkgs) lib; };
 
@@ -94,5 +97,46 @@
           ];
         };
       };
-    };
+
+     darwinConfigurations = {
+       moria = inputs.nix-darwin.lib.darwinSystem {
+         system = "aarch64-darwin";
+         specialArgs = {
+           inherit inputs vars;
+           outputs = self;
+         };
+         modules = [
+           ({ pkgs, ... }: {
+             networking.hostName = "moria";
+
+             # Determinate uses its own daemon to manage the Nix installation that
+             # conflicts with nix-darwin’s native Nix management.
+             # If we do not disable this, we'll get this error:
+             # "error: Determinate detected, aborting activation"
+             # To resolve this, turn off nix-darwin’s management of the Nix
+             # installation. We do that by:
+             nix.enable = false;
+
+             # List packages installed in system profile. To search by name, run:
+             # $ nix-env -qaP | grep wget
+             environment.systemPackages = [
+               pkgs.vim
+               pkgs.tmux
+               pkgs.htop
+             ];
+
+             # Necessary for using flakes on this system.
+             nix.settings.experimental-features = "nix-command flakes";
+
+             system.configurationRevision = self.rev or self.dirtyRev or null;
+
+             # Used for backwards compatibility, please read the changelog before changing.
+             # $ darwin-rebuild changelog
+             system.stateVersion = 6;
+             nixpkgs.hostPlatform = "aarch64-darwin";
+           })
+         ];
+       };
+     };
+   };
 }
