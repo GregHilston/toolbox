1d6d5d3ca9e0457a45062e463ef2086dedf6fab7
saylesss88 <saylesss87@proton.me>
2025-07-12 10:19:23 -0400

    edit vmware-guest

--- nixos/hosts/vms/x86_64/default.nix
+++ nixos/hosts/vms/x86_64/default.nix
@@ -1,25 +1,34 @@
-{ inputs, outputs, lib, config, pkgs, vars, ... }:
-
 {
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  vars,
+  ...
+}: {
   imports = [
     ./hardware-configuration.nix
     ../../../modules/common
+    ./steam.nix
   ];
 
   networking.hostName = "nixos-vm";
 
   boot.loader.grub = {
     enable = true;
-    device = "/dev/sda";  # Typical for VMs
-    useOSProber = false;  # Usually not needed in VMs
+    device = "/dev/sda"; # Typical for VMs
+    useOSProber = false; # Usually not needed in VMs
   };
+
+  gaming.enable = true;
 
   services.spice-vdagentd.enable = true;
   services.qemuGuest.enable = true;
 
   services.openssh = {
     enable = true;
-    ports = [ 22 ];
+    ports = [22];
     settings = {
       PasswordAuthentication = true;
       AllowUsers = null; # Allows all users by default. Can be [ "user1" "user2" ]
