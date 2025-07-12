89fb3c9ee4fbb0d49021e31cb40bc5f339bc47a2
saylesss88 <saylesss87@proton.me>
2025-07-12 17:16:25 -0400

    Add: virtualisation.vmware.guest.enable = true; Only for mines configuration
    Comment Out: Other kernel modules as vmware has it's own specialized
    modules. I left them there for now just in case but if everything
    still works fine we will remove them.

--- nixos/hosts/vms/mines/default.nix
+++ nixos/hosts/vms/mines/default.nix
@@ -7,26 +7,29 @@
     ./hardware-configuration.nix
   ];
 
+  # VMWare Tools
+  virtualisation.vmware.guest.enable = true;
+
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Add these kernel modules for ARM virtualization
   boot.initrd.availableKernelModules = [
-    "virtio_pci" # Virtio PCI devices
-    "virtio_blk" # Block storage (disks)
-    "virtio_net" # Network interfaces
-    "virtio_mmio" # Memory-mapped I/O
+    # "virtio_pci" # Virtio PCI devices
+    # "virtio_blk" # Block storage (disks)
+    # "virtio_net" # Network interfaces
+    # "virtio_mmio" # Memory-mapped I/O
     "ext4" # Root filesystem support
-    "nvme" # If using NVMe storage
+    # "nvme" # If using NVMe storage
   ];
 
   # Ensure virtio modules are included in initrd
-  boot.initrd.kernelModules = [
-    "virtio_pci"
-    "virtio_blk"
-    "virtio_net"
-  ];
+  # boot.initrd.kernelModules = [
+  #   "virtio_pci"
+  #   "virtio_blk"
+  #   "virtio_net"
+  # ];
   # Override the hostname from "nixos-vm" to "mines".
   networking.hostName = lib.mkDefault "mines";
 
