d5e71585bca245722fb76235a660836306cd935d
saylesss88 <saylesss87@proton.me>
2025-07-14 15:33:13 -0400

    add: wpaperd service

--- nixos/modules/programs/gui/sway/wpaperd.nix
+++ nixos/modules/programs/gui/sway/wpaperd.nix
@@ -29,18 +29,18 @@
        transition-time = 600
     '';
   };
-  # systemd.user.services.wpaperd = {
-  #   Unit = {
-  #     description = "wpaperd wallpaper daemon";
-  #     wantedBy = ["sway-session.target"];
-  #     after = ["sway-session.target"];
-  #   };
-  #   Service = {
-  #     Type = "simple";
-  #     ExecStart = "${pkgs.wpaperd}/bin/wpaperd -d";
-  #     ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p ~/.cache/fontconfig"; # Ensure cache directory
-  #     Restart = "on-failure";
-  #     RestartSec = 5;
-  #   };
-  # };
+  systemd.user.services.wpaperd = {
+    Unit = {
+      description = "wpaperd wallpaper daemon";
+      wantedBy = ["sway-session.target"];
+      after = ["sway-session.target"];
+    };
+    Service = {
+      Type = "simple";
+      ExecStart = "${pkgs.wpaperd}/bin/wpaperd -d";
+      ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p ~/.cache/fontconfig"; # Ensure cache directory
+      Restart = "on-failure";
+      RestartSec = 5;
+    };
+  };
 }
