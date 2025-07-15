9369ab76867395e70180b71128a52c1cd17a6c9f
saylesss88 <saylesss87@proton.me>
2025-07-15 15:32:01 -0400

    edit: fix swayidle & swaylock
    Needed swayidle to launch `${pkgs.swaylock-effects}/bin/swaylock`
    rather than `${pkgs.swaylock-effects}/bin/swaylock-effects` for it
    to work correctly.

--- nixos/modules/programs/gui/sway/swayidle.nix
+++ nixos/modules/programs/gui/sway/swayidle.nix
@@ -4,17 +4,17 @@
     events = [
       {
         event = "before-sleep";
-        command = "${pkgs.swaylock-effects}/bin/swaylock-effects";
+        command = "${pkgs.swaylock-effects}/bin/swaylock";
       }
       {
         event = "lock";
-        command = "${pkgs.swaylock-effects}/bin/swaylock-effects";
+        command = "${pkgs.swaylock-effects}/bin/swaylock";
       }
     ];
     timeouts = [
       {
         timeout = 300; # 5 min
-        command = "${pkgs.swaylock-effects}/bin/swaylock-effects -fF";
+        command = "${pkgs.swaylock-effects}/bin/swaylock -fF";
       }
       {
         timeout = 900; # 15 minutes (900 seconds)
