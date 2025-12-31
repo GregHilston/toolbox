c7c326c80ab88d23f0b13fc3f4560ef67dba855a
GregHilston <Gregory.Hilston@gmail.com>
2025-12-30 21:46:12 -0500

    feat: add useful git aliases
    - Quick shortcuts: st, co, br, ci
    - cleanup: Remove merged branches
    - lg/lga: Pretty graph log views
    - unstage, last, root: Useful utilities

--- nixos/modules/programs/tui/git/default.nix
+++ nixos/modules/programs/tui/git/default.nix
@@ -7,5 +7,25 @@
     enable = true;
     userName = "GregHilston";
     userEmail = "Gregory.Hilston@gmail.com";
+
+    aliases = {
+      # Quick shortcuts
+      st = "status";
+      co = "checkout";
+      br = "branch";
+      ci = "commit";
+
+      # Useful commands
+      cleanup = "!git branch --merged | grep -v '\\*\\|master\\|main\\|develop' | xargs -n 1 -r git branch -d";
+      unstage = "reset HEAD --";
+      last = "log -1 HEAD";
+
+      # Pretty log views
+      lg = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative";
+      lga = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative --all";
+
+      # Repository root
+      root = "rev-parse --show-toplevel";
+    };
   };
 }
