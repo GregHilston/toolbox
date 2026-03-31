9b45df1b6ca0aa8da4363d5bcb2ede9c7ddda349
ghilston <Gregory.Hilston@gmail.com>
2026-03-31 10:33:50 -0400

    add direnv permissions to auto run in a few directories

--- nixos/modules/programs/tui/direnv/default.nix
+++ nixos/modules/programs/tui/direnv/default.nix
@@ -1,8 +1,12 @@
 # nixos/modules/programs/tui/direnv/default.nix
-{...}: {
+{vars, ...}: {
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
     enableZshIntegration = true;
+    config.whitelist.prefix = [
+      "/Users/${vars.user.name}/Git/home-lab"
+      "/Users/${vars.user.name}/Git/toolbox"
+    ];
   };
 }
