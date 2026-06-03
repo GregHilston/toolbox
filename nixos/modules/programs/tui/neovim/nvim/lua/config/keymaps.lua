25367b851b78c5f1f9953f3f616d4d3f0b59d769
Greg Hilston <Gregory.Hilston@gmail.com>
2026-06-03 14:43:54 -0400

    fix(nvim): on boot error

--- nixos/modules/programs/tui/neovim/nvim/lua/config/keymaps.lua
+++ nixos/modules/programs/tui/neovim/nvim/lua/config/keymaps.lua
@@ -1,9 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set(
-  "n",
-  "<leader>sx",
-  require("telescope.builtin").resume,
-  { noremap = true, silent = true, desc = "Resume" }
-)
+vim.keymap.set("n", "<leader>sx", function()
+  require("telescope.builtin").resume()
+end, { noremap = true, silent = true, desc = "Resume" })
