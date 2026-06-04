65c59f12deff67457aec3ae9e550191f4ada1ec3
Greg Hilston <Gregory.Hilston@gmail.com>
2026-06-04 12:10:30 -0400

    chore: stop lazy vim from popping up with whats never everytime

--- nixos/modules/programs/tui/neovim/nvim/lua/config/lazy.lua
+++ nixos/modules/programs/tui/neovim/nvim/lua/config/lazy.lua
@@ -17,7 +17,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = { news = { lazyvim = false } } },
     -- import/override with your plugins
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
